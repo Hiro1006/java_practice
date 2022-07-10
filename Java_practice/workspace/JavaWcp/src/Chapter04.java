e9c0ebfb694196a4f9d7a64b80364d6deb426047
Hiro1006 <99570565+Hiro1006@users.noreply.github.com>
2022-07-11 00:39:36 +0900

    最新

---
+++ Java_practice/workspace/JavaWcp/src/Chapter04.java
@@ -0,0 +1,11 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		final int luckey_number = 6;
+		String str = "太田拓人";
+		System.out.println(str);
+		str = "臥薪嘗胆";
+		System.out.println(str);
+		System.out.println(luckey_number);
+	}
+}
